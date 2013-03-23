95e4cc882e33ab961a8a110921a71e47eba4290f
Przemek Owczarek <przemek@futuresimple.com>
2013-03-23 22:40:57 +0100

    Naive C++ fizzbuzz

---
+++ fizzbuzz/cpp/fizzbuzz.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+  for (unsigned int i = 1; i <= 100; i++) {
+    if (i % 15 == 0) {
+      std::cout << "FizzBuzz" << std::endl;
+    } else if (i % 3 == 0) {
+      std::cout << "Fizz" << std::endl;
+    } else if (i % 5 == 0) {
+      std::cout << "Buzz" << std::endl;
+    } else {
+      std::cout << i << std::endl;
+    }
+  }
+
+  return 0;
+}
+
