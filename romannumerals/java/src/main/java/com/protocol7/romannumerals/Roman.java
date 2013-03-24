34619ad80138e88778ea92a25e5fddc903fd96d8
Niklas Gustavsson <niklas@protocol7.com>
2013-03-24 15:37:23 +0100

    A more compacy Roman numerals

--- romannumerals/java/src/main/java/com/protocol7/romannumerals/Roman.java
+++ romannumerals/java/src/main/java/com/protocol7/romannumerals/Roman.java
@@ -1,44 +1,29 @@
 package com.protocol7.romannumerals;
 
-import java.util.Arrays;
 
 public class Roman {
 
-  public String convert(int i) {
-    return fill(i / 1000, 'M')
-      + format(i / 100 % 10, 'C', 'D', 'M')
-      + format(i / 10 % 10, 'X', 'L', 'C')
-      + format(i % 10, 'I', 'V', 'X');
-  }
+  enum Romans {
+    M(1000), CM(900), D(500), CD(400),
+    C(100),  XC(90),  L(50),  XL(40),
+    X(10),   IX(9),   V(5),   IV(4),
+    I(1);
 
-  private String format(int i, char digit, char five, char ten) {
-    switch (i) {
-      case 1:
-      case 2:
-      case 3:
-        return fill(i, digit);
-      case 4:
-        return string(digit, five);
-      case 5:
-        return string(five);
-      case 6:
-      case 7:
-      case 8:
-        return five + fill(i - 5, digit);
-      case 9:
-        return string(digit, ten);
-      default:
-        return "";
+    int i;
+
+    Romans(int i) {
+      this.i = i;
     }
   }
 
-  private String string(char... cs) {
-    return new String(cs);
-  }
-
-  private String fill(int len, char c) {
-    char[] cs = new char[len];
-    Arrays.fill(cs, c);
-    return new String(cs);
+  public String i2r(int i) {
+    StringBuilder sb = new StringBuilder();
+    for (Romans roman : Romans.values()) {
+      while (i >= roman.i) {
+        sb.append(roman.name());
+        i -= roman.i;
+      }
+    }
+    return sb.toString();
   }
 }
--- romannumerals/java/src/test/java/com/protocol7/romannumerals/RomanTest.java
+++ romannumerals/java/src/test/java/com/protocol7/romannumerals/RomanTest.java
@@ -10,52 +10,52 @@ public class RomanTest {
 
   @Test
   public void testSimple() {
-    assertEquals("I", roman.convert(1));
-    assertEquals("V", roman.convert(5));
-    assertEquals("X", roman.convert(10));
-    assertEquals("L", roman.convert(50));
-    assertEquals("C", roman.convert(100));
-    assertEquals("D", roman.convert(500));
-    assertEquals("M", roman.convert(1000));
+    assertEquals("I", roman.i2r(1));
+    assertEquals("V", roman.i2r(5));
+    assertEquals("X", roman.i2r(10));
+    assertEquals("L", roman.i2r(50));
+    assertEquals("C", roman.i2r(100));
+    assertEquals("D", roman.i2r(500));
+    assertEquals("M", roman.i2r(1000));
   }
 
   @Test
   public void testMultiples() {
-    assertEquals("II", roman.convert(2));
-    assertEquals("III", roman.convert(3));
-    assertEquals("CC", roman.convert(200));
-    assertEquals("MM", roman.convert(2000));
+    assertEquals("II", roman.i2r(2));
+    assertEquals("III", roman.i2r(3));
+    assertEquals("CC", roman.i2r(200));
+    assertEquals("MM", roman.i2r(2000));
 
     // on the other hand, some multiples are not ok
-    assertEquals("X", roman.convert(10));
-    assertEquals("C", roman.convert(100));
-    assertEquals("M", roman.convert(1000));
+    assertEquals("X", roman.i2r(10));
+    assertEquals("C", roman.i2r(100));
+    assertEquals("M", roman.i2r(1000));
   }
 
   @Test
   public void testAddingCombinations() {
-    assertEquals("VI", roman.convert(6));
-    assertEquals("VII", roman.convert(7));
-    assertEquals("VIII", roman.convert(8));
-    assertEquals("XI", roman.convert(11));
-    assertEquals("XV", roman.convert(15));
+    assertEquals("VI", roman.i2r(6));
+    assertEquals("VII", roman.i2r(7));
+    assertEquals("VIII", roman.i2r(8));
+    assertEquals("XI", roman.i2r(11));
+    assertEquals("XV", roman.i2r(15));
   }
 
   @Test
   public void testSubtractingCombinations() {
-    assertEquals("IV", roman.convert(4));
-    assertEquals("IX", roman.convert(9));
-    assertEquals("XLIX", roman.convert(49));
+    assertEquals("IV", roman.i2r(4));
+    assertEquals("IX", roman.i2r(9));
+    assertEquals("XLIX", roman.i2r(49));
   }
 
   @Test
   public void test1999() {
-    assertEquals("MCMXCIX", roman.convert(1999));
+    assertEquals("MCMXCIX", roman.i2r(1999));
   }
 
   public void testTooSmall() {
-    assertEquals("", roman.convert(0));
-    assertEquals("", roman.convert(-1));
+    assertEquals("", roman.i2r(0));
+    assertEquals("", roman.i2r(-1));
 
   }
 }
