160c79825aea83901ce1baa09235d8bbee514917
Niklas Gustavsson <niklas@protocol7.com>
2014-05-17 00:17:45 +0200

    Go, take 2

--- fizzbuzz/go/fizzbuzz.go
+++ fizzbuzz/go/fizzbuzz.go
@@ -4,14 +4,11 @@ import "fmt"
 
 func main() {
   for i:= 1; i<101; i++ {
-    if i % 15 == 0 {
-      fmt.Println("FizzBuzz")
-    } else if i % 3 == 0 {
-      fmt.Println("Fizz")
-    } else if i % 5 == 0 {
-      fmt.Println("Buzz")
-    } else {
-      fmt.Println(i)
+    switch {
+      case i % 15 == 0: fmt.Println("FizzBuzz")
+      case i % 3 == 0: fmt.Println("Fizz")
+      case i % 5 == 0: fmt.Println("Buzz")
+      default: fmt.Println(i)
     }
   }
 }
